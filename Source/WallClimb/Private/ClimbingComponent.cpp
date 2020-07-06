472e2a5873249e032dd73e7cee0afb76a978f740
Eduard Korogodov <eduard.korogodov@gmail.com>
2020-07-06 04:34:11 +0300

    Initialization, Tick trace

--- Source/WallClimb/Private/ClimbingComponent.cpp
+++ Source/WallClimb/Private/ClimbingComponent.cpp
@@ -2,6 +2,12 @@
 
 
 #include "ClimbingComponent.h"
+#include "Engine/World.h"
+
+#include "GameFramework/CharacterMovementComponent.h"
+#include "Components/CapsuleComponent.h"
+#include "Components/ArrowComponent.h"
+#include "Components/SkinnedMeshComponent.h"
 
 // Sets default values for this component's properties
 UClimbingComponent::UClimbingComponent()
@@ -10,17 +16,49 @@ UClimbingComponent::UClimbingComponent()
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
 
-	// ...
+	ObjectsToTrace = FCollisionObjectQueryParams::AllStaticObjects;
+	
 }
-
 
 // Called when the game starts
 void UClimbingComponent::BeginPlay()
 {
 	Super::BeginPlay();
 
-	// ...
+	auto Owner = GetOwner();
+	if (!(Owner))
+	{
+		UE_LOG(LogTemp, Error, TEXT("[%s] Use of unintialized pointers."), *FString(__FUNCTION__));
+		return;
+	}
+	auto Comps = Owner->GetComponentsByTag(UArrowComponent::StaticClass(), "TraceArrow");
+	if (Comps.Num() > 0)
+	{
+		ChestBoneSocket = Cast<UArrowComponent>(Comps[0]);
+	}
+
+	MovementComp = Cast<UCharacterMovementComponent>(Owner->GetComponentByClass(UCharacterMovementComponent::StaticClass()));
 	
+}
+
+void UClimbingComponent::OnMoveRight_Implementation()
+{
+	// For later
+}
+
+void UClimbingComponent::OnHangRelease_Implementation()
+{
+	// For later
+}
+
+void UClimbingComponent::OnJumpPressed_Implementation()
+{
+	// For later
+}
+
+void UClimbingComponent::OnJumpReleased_Implementation()
+{
+	// For later
 }
 
 
@@ -29,6 +67,101 @@ void UClimbingComponent::TickComponent(float DeltaTime, ELevelTick TickType, FAc
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-	// ...
+	/* TODO: Tick freeze for UpdateClimbingData to be added, for when climbing is not needed */
+	UpdateClimbingData();
 }
 
+bool UClimbingComponent::IsClimbable(const FHitResult & HitResult)
+{
+	auto Owner = GetOwner();
+
+	if (!(Owner && MovementComp))
+	{
+		UE_LOG(LogTemp, Error, TEXT("[%s] Use of unintialized pointers."), *FString(__FUNCTION__));
+		return false;
+	}
+
+	/* Walkable surface check*/
+	if (MovementComp->IsWalkable(HitResult))
+	{
+		return false;
+	}
+
+	/* Negative slope check*/
+	if ((HitResult.ImpactNormal.Z > -0.05f))
+	{
+		return false;
+	}
+
+	auto ActorFwdVec = Owner->GetActorForwardVector();
+	float Dot = FVector2D::DotProduct(FVector2D(ActorFwdVec.X, ActorFwdVec.Y), 
+		FVector2D(HitResult.ImpactNormal.X, HitResult.ImpactNormal.Y));
+	
+	/* Climable tilt check */
+	if ((FMath::Acos(Dot) > (180.f - MaxSurfaceCaptureAngle)))
+	{
+		return false;
+	}
+
+	return true;
+}
+
+void UClimbingComponent::OnCharacterLanded_Implementation()
+{
+	// For later
+}
+
+bool UClimbingComponent::CanStartClimbing()
+{
+	// Check if we are allowed to climb, have something to climb and not climbing already
+	return HasAbilityToClimb && IsClimbOnHitAllowed 
+		&& ( ActorToClimbOn != nullptr) && !IsOnTheWall() ;
+}
+
+bool UClimbingComponent::IsOnTheWall()
+{
+	return IsClimbing || IsHanging;
+}
+
+bool UClimbingComponent::TickTrace(FHitResult & outHitResult)
+{
+	auto Owner = GetOwner();
+
+	if (!(Owner && CapsuleComp))
+	{
+		UE_LOG(LogTemp, Error, TEXT("[%s] Use of unintialized pointers."), *FString(__FUNCTION__));
+		return false;
+	}
+
+	float CapsuleHalfHeight, CapsuleRadius;
+	auto ActorRot = Owner->GetActorRotation().Quaternion();
+	auto ActorLoc = Owner->GetActorLocation();
+	auto ActorForwardVector = Owner->GetActorForwardVector();
+
+	CapsuleComp->GetScaledCapsuleSize(CapsuleRadius, CapsuleHalfHeight);
+
+	FCollisionShape CollisionBox = FCollisionShape::MakeBox(FVector(CapsuleRadius, CapsuleRadius, CapsuleHalfHeight * 0.75f));
+
+	FVector TraceCenter = ActorLoc + (ActorForwardVector * (CapsuleRadius * 1.5f));
+	
+	/* TODO: Should be branched with Line trace if IsHanging later. */
+	return GetWorld()->SweepSingleByObjectType(outHitResult, TraceCenter, TraceCenter, ActorRot, ObjectsToTrace, CollisionBox);
+}
+
+void UClimbingComponent::UpdateClimbingData()
+{
+	FHitResult HitResult;
+
+	if (! TickTrace(HitResult))
+	{
+		/* TODO: Get rid of this ugly work around */
+		if (!IsHanging)
+		{
+			ActorToClimbOn = nullptr;
+			CurrentSurfaceNormal = FVector::ZeroVector;
+		}
+	}
+
+	ActorToClimbOn = HitResult.GetActor();
+	CurrentSurfaceNormal = HitResult.ImpactNormal;
+}
--- Source/WallClimb/Public/ClimbingComponent.h
+++ Source/WallClimb/Public/ClimbingComponent.h
@@ -6,7 +6,20 @@
 #include "Components/ActorComponent.h"
 #include "ClimbingComponent.generated.h"
 
-UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+UENUM()
+enum class EClimbDirection : uint8
+{
+	UPWARDS					UMETA(DisplayName = "Upwards"),
+	LEFT					UMETA(DisplayName = "Left"),
+	LEFT_ARROUND_CORNER		UMETA(DisplayName = "Left around corner"),
+	RIGHT					UMETA(DisplayName = "Right"),
+	RIGHT_AROUND_CORNER		UMETA(DisplayName = "Right around corner"),
+	UP_ON					UMETA(DisplayName = "Up on"),
+	IDLE					UMETA(DisplayName = "Idle hanging"),
+	NONE					UMETA(DisplayName = "None")
+};
+
+UCLASS( Blueprintable, ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class WALLCLIMB_API UClimbingComponent : public UActorComponent
 {
 	GENERATED_BODY()
@@ -19,9 +32,104 @@ protected:
 	// Called when the game starts
 	virtual void BeginPlay() override;
 
+	/** Has to be triggered when the player presses Jump */
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category = "Event")
+	void OnJumpPressed();
+
+	/** Has to be triggered when the player releases Jump */
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category = "Event")
+	void OnJumpReleased();
+
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category = "Event")
+	void OnMoveRight();
+
+	/** Has to be triggered when the player presses Realese */
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category = "Event")
+	void OnHangRelease();
+
+	/** Has to be triggered on Character's OnLanded */
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category = "Event")
+	void OnCharacterLanded();
+
+	/** A set of checks before starting a climb */
+	bool CanStartClimbing();
+
+	/** Climbing states check */
+	bool IsOnTheWall();
+
+	bool TickTrace(FHitResult& outHitResult);
+
+	void UpdateClimbingData();
+
+	void StartClimbing();
+
+	void StopClimbing();
+
+	void UpdateClimbing(float DeltaTime);
+
+	void StartHanging();
+
+	void StopHanging();
+
+	void UpdateHanging(float DeltaTime);
+
+	
+
+	void LocationTransition();
+
 public:	
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
-		
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Climbing|Values", meta = (DisplayName = "Is Climbing"))
+	bool IsClimbing;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Climbing|Values", meta = (DisplayName = "Is Climbing"))
+	bool IsHanging;
+
+	/** For animation state transition purpose */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Climbing|Values", meta = (DisplayName = "Climbing Direction"))
+	EClimbDirection ClimbingDirection;
+
+	/** A marker to define the height of the character's chest 
+		TODO: Replace with USkeletalMeshSocket */
+	UPROPERTY(BlueprintReadWrite, EditDefaultsOnly, Category = "Climbing|Setup", meta = (DisplayName = "Chest bone socket Ref"))
+	class UArrowComponent* ChestBoneSocket;
+
+	UPROPERTY(BlueprintReadWrite, EditDefaultsOnly, Category = "Climbing|Setup", meta = (DisplayName = "Capsule Component Ref"))
+	class UCapsuleComponent* CapsuleComp;
+
+	UPROPERTY(BlueprintReadWrite, EditDefaultsOnly, Category = "Climbing|Setup", meta = (DisplayName = "Movement Component Ref"))
+	class UCharacterMovementComponent* MovementComp;
+
+	/** Max height that can be climbed */
+	UPROPERTY(BlueprintReadWrite, EditDefaultsOnly, Category = "Climbing|Setup", meta = (DisplayName = "Max Climbing Distance"))
+	float MaxClimbingDistance = 200;
+
+	/** An angle of capturing a surface */
+	UPROPERTY(BlueprintReadWrite, EditDefaultsOnly, Category = "Climbing|Setup", meta = (DisplayName = "Max Capture Angle", ClampMin = "0", ClampMax = "45"))
+	float MaxSurfaceCaptureAngle = 45.f;
+
+	UPROPERTY(BlueprintReadWrite, EditDefaultsOnly, Category = "Climbing|Setup", meta = (DisplayName = "Climb on hit allowed"))
+	bool IsClimbOnHitAllowed;
+
+private:
+	bool CanStartHanging;
+
+	/** Resets on touching the groung, or hanging */
+	bool HasAbilityToClimb;
+
+	/** An actor to climb on*/
+	AActor* ActorToClimbOn;
+	
+	/** A surface we are currently operating with */
+	FVector CurrentSurfaceNormal;
+
+	/** Filter for traces for objects */
+	FCollisionObjectQueryParams ObjectsToTrace;
+
+private:
+
+	bool IsClimbable(const FHitResult& HitResult);
 };
